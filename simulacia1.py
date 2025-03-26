014f6df4bd67e2d9a251592f774c086c540299db
Sara Geratova <geratova@isw.sk>
2025-03-26 13:50:13 +0100

    simulacia1 fix

--- simulacia1.py
+++ simulacia1.py
@@ -6,17 +6,23 @@
 # qt pocet uloh v rade v case t (v tejto simulacii -> l(priemer) = 1/T suma od 1 do n z di)
 # xt pocet uloh v servise v case t (v tejto simulacii -> l(priemer) = 1/T suma od 1 do n z si)
 
-# a = 15, 47, 71, 111, 123, 152, 166, 226, 310, 320
-# s = 43, 36, 34, 30, 38, 40, 31, 29, 36, 30
+# a = 15, 47, 71, 111, 123, 152, 166, 226, 310, 320 -> cas prichodu, kedy pride uloha do uzlu
+# b -> kedy uloha zacne byt obsluhovana
+# c -> kedy uloha skonci byt obsluhovana
+# d -> ako dlho caka uloha na obsluhu
+# s = 43, 36, 34, 30, 38, 40, 31, 29, 36, 30 -> ako dlho trva obsluha ulohy
 # potom by sme mali dostat -> d = 0, 11, 23, 17, 35, 44, 70, 41, 0, 26
 
 # Given arrival times and service times
 a = [15, 47, 71, 111, 123, 152, 166, 226, 310, 320]
 s = [43, 36, 34, 30, 38, 40, 31, 29, 36, 30]
+w = [] # -> celkovy cas ktory uloha stravi v uzle
+d = []
 
 # Initialize completion times and waiting times
 c = [0] * len(a)
 d = [0] * len(a)
+w = [0] * len(a)
 
 i = 0
 while i < len(a):
@@ -26,11 +32,13 @@ while i < len(a):
         c[i] = max(a[i], c[i - 1]) + s[i]  # Service starts after the previous completion
 
     d[i] = c[i] - a[i] - s[i]  # Waiting time
+    w[i] = d[i] + s[i]
     i += 1
 
 # Output results
 print("Completion times:", c)
 print("Waiting times:", d)
+print("Waiting times:", w)
 
 # # Initialize completion times and waiting times
 # c = [0] * len(a)
