5021bd0490dc785187460cdd2e731a7a2b154324
Sara Geratova <geratova@isw.sk>
2025-04-02 15:53:11 +0200

    feat(cv7): description

---
+++ cv7/viacserverovyUzol.py
@@ -0,0 +1,9 @@
+# q = l - suma od 1 po c z xi
+#pridate do uzla
+# 1. l = l + 1 (l je pocet jobov v uzle)
+# ak l <= s -> priradit na server
+# inak -> pridat do radu
+# odchod zo servera
+# 1. l = l - 1
+# ak l >= c (situacia ze niekto cakal v rade) -> vyber z rady
+
